70cff15afb1ed89ece845734ad58cb3c145bd20c
Alex Ting <santinoaa@gmail.com>
2021-04-09 13:12:33 +0800

    initial device

---
+++ devices.py
@@ -0,0 +1,24 @@
+cisco_ios = {
+    'device_type': 'cisco_ios_ssh',
+    'ip': '192.168.122.10',
+    'username': 'alex',
+    'password': 'alex',
+    'port': 22,
+}
+
+cisco_xr = {
+    'device_type': 'cisco_xr_ssh',
+    'ip': '10.10.10.27',
+    'username': 'admin',
+    'password': 'passwd',
+    'port': 9722,
+}
+
+arista_veos = {
+    'device_type': 'arista_eos_ssh',
+    'ip': '10.10.10.27',
+    'username': 'admin',
+    'password': 'passwd',
+    'secret': '',
+    'port': 8622,
+}
