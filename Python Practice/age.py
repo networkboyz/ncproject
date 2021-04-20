cd62463dc3330a4bf7b6af763d920c0fac8c3ce3
Alex Ting <santinoaa@gmail.com>
2021-04-20 23:45:28 +0800

    practice

---
+++ Python Practice/age.py
@@ -0,0 +1,13 @@
+#this is a test program
+
+print('Hello World')
+print('What is your name?')
+myName = input()
+print ('nice to meet you, '+ myName)
+print ('the length of your name is: ')
+print (len(myName))
+print ('What is your age?')
+myAge = input()
+print ('you will be ' + str(int(myAge) + 1) + ' in a year.')
+test github45
+
